fbdec044b878bd3c48ca97cbc7e99dc06c0ed3bf
Devyash Saini <dysaini2004@gmail.com>
2026-04-28 23:13:55 +0530

    cors
    Signed-off-by: Devyash Saini <dysaini2004@gmail.com>

--- backend/app/main.py
+++ backend/app/main.py
@@ -34,7 +34,13 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://flipkart-with-a-c.vercel.app",
+        "https://flipkart-with-a-c.thedevyash.tech",
+        "https://flipkart-with-a-c.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
